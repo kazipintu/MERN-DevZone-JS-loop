03c9c85b2911ffbfd5d60f4ee415c40bcc1d5ceb
kazipintu <kazipintu@gmail.com>
2024-05-13 21:22:01 +0530

    MERN/Devzone/JS/loop

---
+++ loop.js
@@ -0,0 +1,27 @@
+/*  ~ for loop, while loop, do while loop ~  */
+
+/* FOR LOOP */
+for (let i = 1; i <= 10; i++) {
+  console.log("MERN");
+}
+
+// infinite loop will run without any condition.
+
+/* WHILE LOOP */
+{
+  let i = 1;
+  while (i <= 5) {
+    console.log("i = ", i);
+    i++;
+  }
+}
+
+/* DO WHILE LOOP */
+
+{
+  let i = 1;
+  do {
+    console.log("i = ", i);
+    i++;
+  } while (i <= 10);
+}
